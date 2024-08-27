3ee3af29cbbb365829cba068c141cf55684c9504
tilli <Zvada_A_S@itgri.ru>
2024-08-27 22:38:14 +0300

    File test Employee 3.5

--- src/Employee/task3-5.php
+++ src/Employee/task3-5.php
@@ -3,11 +3,12 @@ require_once "/var/www/html/vendor/autoload.php";
 
 use CleanCode\Employee\EmployeeFactoryImpl;
 use CleanCode\Employee\EmployeeRecord;
+use CleanCode\Employee\EmployeeType;
 
 $employeeRecords = [
-    new EmployeeRecord('COMMISSIONED'),
-    new EmployeeRecord('HOURLY'),
-    new EmployeeRecord('SALARIED'),
+    new EmployeeRecord(EmployeeType::COMMISSIONED),
+    new EmployeeRecord(EmployeeType::HOURLY),
+    new EmployeeRecord(EmployeeType::SALARIED),
 ];
 
 $factory = new EmployeeFactoryImpl();
