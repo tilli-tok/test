472496fbe7efd9d2cd6b2c19def8a75773eb5de4
Powernic <powernic1@gmail.com>
2024-09-16 08:38:21 +0300

    6.7 improve

--- src/Demetra/Address.php
+++ src/Demetra/Address.php
@@ -1,34 +1,43 @@
 <?php
 declare(strict_types=1);
+
 namespace CleanCode\Demetra;
 
 
-class Address{
-    private string $street;
-    private ?string $streetExtra;
-    private string $city;
-    private string $state;
-    private string $zip;
-    public function __construct(string $street, string $streetExtra, string $city, string $state, string $zip){
-        $this->street = $street;
-        $this->streetExtra = $streetExtra;
-        $this->city = $city;
-        $this->state = $state;
-        $this->zip = $zip;
+readonly class Address
+{
+    public function __construct
+    (
+        private string $street,
+        private string $streetExtra,
+        private string $city,
+        private string $state,
+        private string $zip)
+    {
     }
-    public function getStreet(): string{
+
+    public function getStreet(): string
+    {
         return $this->street;
     }
-    public function getStreetExtra(): ?string{
+
+    public function getStreetExtra(): string
+    {
         return $this->streetExtra;
     }
-    public function getCity(): string{
+
+    public function getCity(): string
+    {
         return $this->city;
     }
-    public function getState(): string{
+
+    public function getState(): string
+    {
         return $this->state;
     }
-    public function getZip(): string{
+
+    public function getZip(): string
+    {
         return $this->zip;
     }
 }
