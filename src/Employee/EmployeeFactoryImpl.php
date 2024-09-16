c03be6bdc5a70845192ee83fb8453b293bf4ebdb
Powernic <powernic1@gmail.com>
2024-09-16 08:24:23 +0300

    3.5 improve

--- src/Employee/EmployeeFactoryImpl.php
+++ src/Employee/EmployeeFactoryImpl.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 namespace CleanCode\Employee;
 
 class EmployeeFactoryImpl implements EmployeeFactory {
+
     /**
-     * @param EmployeeRecord $r
-     * @return CommissionedEmployee|HourlyEmployee|SalariedEmployee
      * @throws InvalidEmployeeType
      */
-    public function makeEmployee(EmployeeRecord $r): CommissionedEmployee|HourlyEmployee|SalariedEmployee
+    public function makeEmployee(EmployeeRecord $r): Employee
     {
         return match ($r->type) {
             EmployeeType::COMMISSIONED => new CommissionedEmployee($r),
