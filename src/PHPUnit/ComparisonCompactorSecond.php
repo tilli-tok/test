dd0092a09b1b1796ce52991741c679e9ebaf7d85
tilli <Zvada_A_S@itgri.ru>
2024-09-18 09:46:47 +0300

    listing 15.3

--- src/PHPUnit/ComparisonCompactorSecond.php
+++ src/PHPUnit/ComparisonCompactorSecond.php
@@ -5,12 +5,12 @@ namespace CleanCode\PHPUnit;
 class ComparisonCompactorSecond
 {
     private int $ctxt;
-    private string $s1;
-    private string $s2;
+    private ?string $s1;
+    private ?string $s2;
     private int $pfx;
     private int $sfx;
 
-    public function __construct(int $ctxt, string $s1, string $s2)
+    public function __construct(int $ctxt, ?string $s1, ?string $s2)
     {
         $this->ctxt = $ctxt;
         $this->s1 = $s1;
@@ -19,12 +19,12 @@ class ComparisonCompactorSecond
 
     public function compact(string $msg): string
     {
-        if ($this->s1 === null || $this->s2 === null || $this->s1 === $this->s2) {
+        if ($this->s1 === null || $this->s2 === null  || $this->areStringsEqual()) {
             return $this->format($msg, $this->s1, $this->s2);
         }
 
         $this->pfx = 0;
-        for (; $this->pfx < strlen($msg); $this->pfx++) {
+        for (; $this->pfx < min(strlen($this->s1),strlen($this->s2)); $this->pfx++) {
             if($this->s1[$this->pfx] !== $this->s2[$this->pfx]){
                 break;
             }
@@ -33,7 +33,7 @@ class ComparisonCompactorSecond
         $sfx1 = strlen($this->s1) - 1;
         $sfx2 = strlen($this->s2) - 1;
 
-        for (; $sfx2 >= $this->pfx && $sfx1 >= $this->pfx;$this->pfx--){
+        for (; $sfx2 >= $this->pfx && $sfx1 >= $this->pfx;$sfx1--,$sfx2--){
             if($this->s1[$sfx1] !== $this->s2[$sfx2]) {
                 break;
             }
@@ -49,27 +49,38 @@ class ComparisonCompactorSecond
 
     private function compactString(string $s): string
     {
-        $result = "[" . substr($s, $this->pfx, strlen($s) - $this->sfx + 1) . "]";
+        $result = "[" . $this->substring($s, $this->pfx, strlen($s) - $this->sfx + 1) . "]";
 
         if ($this->pfx > 0) {
-            //И внезапно $s1
             $result = ($this->pfx > $this->ctxt ? "..." : "") .
-                substr($s, max(0, $this->pfx - $this->ctxt), $this->pfx) .
+                $this->substring($this->s1, max(0, $this->pfx - $this->ctxt), $this->pfx) .
                 $result;
         }
 
         if ($this->sfx > 0) {
-            $end = min(strlen($s) - $this->sfx + 1 + $this->ctxt, strlen($s));
-            $result .= substr($s, strlen($s) - $this->sfx + 1, $end - (strlen($s) - $this->sfx + 1)) .
-                (strlen($s) - $this->sfx + 1 < strlen($s) - $this->ctxt ? "..." : "");
+            $end = min(strlen($this->s1) - $this->sfx + 1 + $this->ctxt, strlen($this->s1));
+            $result .= $this->substring($this->s1, strlen($this->s1) - $this->sfx + 1, $end) .
+                (strlen($this->s1) - $this->sfx + 1 < strlen($this->s1) - $this->ctxt ? "..." : "");
         }
 
         return $result;
     }
 
-    private function format(string $msg, string $s1, string $s2): string
+    private function format(?string $msg, ?string $s1, ?string $s2): string
     {
-        return trim(sprintf('%s expected:<%s> but was:<%s>', $msg, $s1, $s2));
+        return trim(sprintf('%s expected:<%s> but was:<%s>', $msg, $s1 ?? 'null', $s2 ?? 'null'));
+    }
+    function substring(string $str, int $start, int $end = null): string
+    {
+        if ($end !== null) {
+            return substr($str, $start, $end - $start);
+        }
+
+        return substr($str, $start);
     }
 
+    private function areStringsEqual(): bool
+    {
+        return $this->s1 === $this->s2;
+    }
 }
